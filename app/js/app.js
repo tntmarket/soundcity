70bfd28b16ce792da2afc0eb24831c7ffbf23415
tntmarket <dave792321001730003212345@gmail.com>
2013-04-06 01:36:02 -0400

    - Let user load local files, and play them by double click or clicking table icon - removed songs.json

--- app/js/app.js
+++ app/js/app.js
@@ -1,43 +1,50 @@
-angular.module('file', []).
-   directive('fileUpload', function() {
+(function() {
 
+var file = angular.module('file', []);
+file.directive('fileUpload', function() {
    return {
       restrict: 'E',
       template: '<input id="song-upload" type="file"'+
                 'webkitdirectory directory multiple mozdirectory>',
       link: function(scope, el, attr, controller) {
          $('#song-upload').change(function(e) {
-            if(attr.ngModel) {
-               scope[attr.ngModel] = e.target.files;
-            }
-            scope.$eval(attr.ngChange);
+            scope.$apply(function() {
+               if(attr.ngModel) {
+                  scope[attr.ngModel] = 
+                     Array.prototype.slice.call(e.target.files);
+               }
+               scope.$eval(attr.ngChange);
+            });
          });
       }
    };
 });
 
+
 angular.module('soundCity', ['file']).
    config(['$routeProvider', function($routeProvider) {
 
    $routeProvider.
-   when('/playing', {
+      when('/playing', {
       templateUrl: 'partials/playing.html',
       controller: PlayingCtrl
    }).
-   when('/mymusic', {
+      when('/mymusic', {
       templateUrl: 'partials/mymusic.html',
       controller: MyMusicCtrl
    }).
-   when('/song/:id', {
+      when('/song/:id', {
       templateUrl: 'partials/song.html',
       controller: SongCtrl
    }).
-   when('/artist/:id', {
+      when('/artist/:id', {
       templateUrl: 'partials/artist.html',
       controller: ArtistCtrl
    }).
-   when('/album/:id', {
+      when('/album/:id', {
       templateUrl: 'partials/album.html',
       controller: AlbumCtrl 
-   });
+   }).
+      otherwise({ redirectTo: '/playing' });
 }]);
+})();
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,12 +1,34 @@
-function MainCtrl($scope) {
+function MainCtrl($rootScope, $scope) {
+   $rootScope.SETSONG = function(song) {
+      if(!song.file) {
+         throw 'SETSONG assumes a valid song param';
+      }
+
+      $rootScope.PLAYING = song;
+
+      if($rootScope.PLAYING) {
+         $rootScope.STOP();
+         URL.revokeObjectURL($rootScope.PLAYING.url);
+      }
+
+      song.url = URL.createObjectURL(song.file);
+      $scope.PLAYER.jPlayer('setMedia', {
+         mp3: song.url
+      });
+
+   };
+
+   $rootScope.PLAY = function() {
+      $scope.PLAYER.jPlayer('play');
+   };
+
+   $rootScope.STOP = function() {
+      $scope.PLAYER.jPlayer('stop');
+   };
+
    $scope.InitJPlayer = function() {
       $(document).ready(function(){
-         $("#jplayer").jPlayer({
-            ready: function(){
-               $(this).jPlayer('setMedia', {
-                  mp3: '/music/Madoka%20Magica/Madoka%20Kaname%20-%20See%20You%20Tomorrow.mp3'
-               });
-            },
+         $scope.PLAYER = $("#jplayer").jPlayer({
             volume: '0.5',
             swfPath: '/app/lib/jplayer/jplayer.swf',
             cssSelectorAncestor: '.player-container',
@@ -24,18 +46,65 @@ function MainCtrl($scope) {
             });
          });
       });
+
+   };
+
+}
+
+function PlayingCtrl($rootScope, $scope, $filter) {
+   $scope.fileExtension = function(filename) {
+      if( filename.indexOf('.') > -1 ) {
+         return filename.split('.').pop();
+      } else {
+         return '';
+      }
+   };
+
+   $scope.isValidMp3 = function(file) {
+      var size = file.size || file.fileSize || 4096;
+      return (size > 4095) && ($scope.fileExtension(file.name) === 'mp3');
+   };
+
+   $scope.getMetadata = function(files) {
+      $.each(files, function(index, file) {
+         var reader = new FileReader();
+
+         reader.onload = function(e) {
+            var responseText = this.result;
+            var dv = new jDataView(responseText);
+
+            if (dv.getString(3, dv.byteLength - 128) == 'TAG') {
+               $scope.$apply(function() {
+                  $scope.songs.push({
+                     name: dv.getString(30, dv.tell()),
+                     artist: dv.getString(30, dv.tell()),
+                     album: dv.getString(30, dv.tell()),
+                     year: dv.getString(4, dv.tell()),
+                     file: file
+                  });
+               });
+            }
+         };
+
+         reader.readAsArrayBuffer(file);
+      });
+   };
+
+   $scope.songs = [];
+
+   $scope.uploadSongs = function() {
+      $scope.files = $filter('filter')($scope.files, $scope.isValidMp3);
+      $scope.getMetadata($scope.files);
+   };
+
+   $scope.playSong = function(song) {
+      $rootScope.SETSONG(song);
+      $rootScope.PLAY();
    };
 }
 
-function PlayingCtrl($scope, $http) {
-   $http.get('/app/data/songs.json').success(function(data) {
-      $scope.songs = data;
-   });
-
-   $scope.uploadSongs = function() {
-      console.log('shitshitshit');
-      console.log($scope.files);
-   };
+function SongRowCtrl($scope) {
+   $scope.song = $routeParams.id;
 }
 
 function SongCtrl($scope, $routeParams) {
